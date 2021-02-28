c456033e8dedcd7d5821489227df48c854dfb01e
Maximilian Schleich <mjschleich@gmail.com>
2021-02-28 15:59:18 -0800

    update ground truth exp

--- scripts/credit/credit_constraints_MACE_counter.jl
+++
@@ -1,6 +0,0 @@
-p = initPLAF()
-
-@PLAF(p, :cf.isMale .== :x.isMale)
-@PLAF(p, :cf.isMarried .== :x.isMarried)
-@PLAF(p, :cf.EducationLevel .<= :x.EducationLevel)
-@PLAF(p, :cf.HasHistoryOfOverduePayments .<= :x.HasHistoryOfOverduePayments)

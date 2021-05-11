172139974231d948a6cd82ae53356abc01514837
GibbsG <zg44@uw.edu>
2021-05-11 01:32:03 -0700

    change functions to GeCo

--- notebook.jl
+++ notebook.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.14.3
+# v0.12.21
 
 using Markdown
 using InteractiveUtils
@@ -64,12 +64,12 @@ begin
 			cf.AgeGroup == 2 end )
 	
 	# If MonthsWithLowSpendingOverLast6Months increases then 	MonthsWithHighSpendingOverLast6Months should decrease
+
+	
 	@PLAF(plaf_prog, 
-		if cf.MonthsWithLowSpendingOverLast6Months .> 
-			x.MonthsWithLowSpendingOverLast6Months;
-			cf.MonthsWithHighSpendingOverLast6Months .<
-			x.MonthsWithHighSpendingOverLast6Months
-	end)
+		if cf.TotalOverdueCounts .> 
+			x.TotalOverdueCounts;
+			cf.MaxBillAmountOverLast6Months .> x.MaxBillAmountOverLast6Months - x.TotalOverdueCounts*3 end)
 end
 
 # ╔═╡ 08034110-b5a7-44c6-8a71-725998b43f60
@@ -122,225 +122,7 @@ Show counterfactual: $(@bind cf_index NumberField(1:nrow(explanations); default=
 if goodness 
 	md"The provided instance does not require an explanation."
 else 
-	# Markdown.parse(GeCo.actions(explanations, instance; num_actions=actions, output="md"))
- 	Markdown.parse(GeCo.actionsDemo(explanations, instance)[cf_index])
-end
-
-# ╔═╡ 0bc25628-9d29-11eb-2d22-070e269ea036
-begin
-	function generate_explantions(X, classifier, p, K)
-		
-		# the result list -> one for each feature
-		num_rows = nrow(X)
-		num_features = ncol(X)
-		
-		weights = zeros(num_features)
-		
-		counts = zeros(num_features)
-		pos_counts = zeros(num_features)
-		neg_counts = zeros(num_features)
-		
-		cum_change = zeros(num_features)
-		pos_cum_change = zeros(num_features)
-		neg_cum_change = zeros(num_features)
-		
-
-		preds = MLJ.pdf.(MLJ.predict(classifier, X),1)
-		instances = X[preds .>= 0.5, :][1:100, :]
-
-		for cur_ins in eachrow(instances)
-			explanations,  = explain(cur_ins, X, p, classifier)
-			for i in 1:K
-				explanation = explanations[i, :]
-				indices = findall(explanation[:mod])
-				for index in indices
-					weights[index] += 1.0/length(indices)
-					counts[index] += 1
-					cum_change[index] += abs(cur_ins[index] - explanation[index])
-					if (explanation[index] > cur_ins[index])
-						pos_counts[index] += 1
-						pos_cum_change[index] += 
-							abs(cur_ins[index] - explanation[index])
-					else
-						neg_counts[index] += 1
-						neg_cum_change[index] += 
-							abs(cur_ins[index] - explanation[index])
-					end
-
-				end
-			end
-		end
-		
-		return instances, weights, counts, cum_change, pos_counts, neg_counts, 
-			pos_cum_change, neg_cum_change
-	end
-
-	instances, weights, counts, cum_change, pos_counts, neg_counts, pos_cum_change, neg_cum_change = generate_explantions(X, classifier, plaf_prog, 5)
-	
-	 
-	md""" ## Explanations for multiple instances
-	"""
-end
-
-# ╔═╡ 1113b964-9d29-11eb-19db-d1c5cb43d340
-instances[1:100,:]
-
-# ╔═╡ a5043111-1600-4d6d-ac55-185d124abca9
-Plots.bar(1:length(counts), counts,
-	xticks=(1:length(counts), names(instance)),
-	xrotation = 30,
-	framestyle = :box,
-	ylabel="Frequency",
-	label="",
-	)
-
-# ╔═╡ 1954ed36-7393-4ff6-843a-17512f79a92b
-
-groupedbar([pos_counts neg_counts],
-	xticks=(1:length(counts), names(instance)),
-	xrotation = 30,
-	framestyle = :box,
-	ylabel="Frequency",
-	label=["Positive Change" "Negative Change"],
-	)
-
-
-# ╔═╡ 8f29bd6c-9d29-11eb-0e06-d9b5178cbe33
-let 
-	ave_change = [
-		(counts[i] == 0) ? 0 : cum_change[i]/counts[i] 
-		for i in 1:length(instance)
-	]
-
-	Plots.bar(1:length(ave_change), ave_change,
-	xticks=(1:length(ave_change), names(instance)),
-	xrotation = 30,
-	framestyle = :box,
-	ylabel="l1-distance",
-	title="Average Absolute Change",
-	label="",
-	)
-end
-
-# ╔═╡ 80f49634-9d2b-11eb-359c-032422eaa82a
-# pos_counts, neg_counts, pos_cum_change, neg_cum_change
-let 
-	pos_changes = [
-		(pos_counts[i] == 0) ? 0 : pos_cum_change[i]/pos_counts[i] 
-		for i in 1:length(instance)
-	]
-	neg_changes = [
-		( neg_counts[i] == 0 ) ? 0 : neg_cum_change[i]/neg_counts[i]
-		for i in 1:length(instance)
-	]
-
-	groupedbar([pos_counts neg_counts],
-		xticks=(1:length(counts), names(instance)),
-		xrotation = 30,
-		framestyle = :box,
-		ylabel="Frequency",
-		# title="Average Changed For Each Feature",
-		label=["Positive Change" "Negative Change"],
-		)
-end
-
-# ╔═╡ 95490a61-0aa7-40f0-aa78-da9a2db11852
-md"### Auxiliary Functions"
-
-# ╔═╡ a70ab516-913b-11eb-2f4c-5b3e31e2d669
-begin 
-	function get_group(user_explanations, k, ori_instance)
-		# sort!(user_explanations, :score)
-
-		groups = Vector{DataFrame}()
-		group_explanations = groupby(user_explanations, :mod)
-
-		for (group_index,group) in enumerate(group_explanations)
-			explanations_g = group[:,1:14]
-			explanations_g = explanations_g[:, filter(x -> x = true, group[1, :mod])]
-
-			# we should filter out the rows that is dominate by others
-			row_num = 1
-
-			keep_rows = trues(nrow(explanations_g))
-
-			for (row_num, cur_instance) in enumerate(eachrow(explanations_g))
-				# check if this row is dominated by previous rows and groups
-
-				dominatedrow = false
-
-				# check for each of the previous groups
-				for prev_group in groups
-
-					if !issubset(propertynames(prev_group), 
-							propertynames(explanations_g))
-						continue
-					end
-
-					for prev_group_row in eachrow(prev_group)
-						isdominated = true
-						for feature in propertynames(prev_group)
-							if !(ori_instance[feature] < 
-									prev_group_row[feature] <=
-									cur_instance[feature] 
-									||  
-									ori_instance[feature] > 
-									prev_group_row[feature] >= 
-									cur_instance[feature])
-								isdominated = false 
-								break
-							end
-						end
-						if isdominated
-							dominatedrow = true
-							break
-						end
-					end
-					if dominatedrow
-						break
-					end
-				end
-				if dominatedrow
-					keep_rows[row_num] = false 
-					continue
-				end
-
-				# check for each of the previous rows
-				for prev_row in 1:row_num-1
-					prev_instance = explanations_g[prev_row,:]
-					isdominated = true
-					for feature in propertynames(explanations_g)
-						if !(ori_instance[feature] <
-								prev_instance[feature] <= 
-								cur_instance[feature] 
-							||  
-								ori_instance[feature] > 
-								prev_instance[feature] >= 
-								cur_instance[feature]
-							)
-							isdominated = false
-							break
-						end
-					end
-					if isdominated
-						dominatedrow = true
-						break
-					end
-				end
-				if dominatedrow
-					keep_rows[row_num] = false 
-				end
-			end
-
-			explanations_g = unique(explanations_g[keep_rows, :])
-			if !isempty(explanations_g)
-				push!(groups, explanations_g)
-			end
-		end
-		return groups
-	end
-	
-	md"get group"
+ 	Markdown.parse(GeCo.actions(DataFrame(explanations[cf_index,:]), instance; num_actions = 2, output = "md"))
 end
 
 # ╔═╡ fa8320c0-1759-4f16-83ca-8329493b9dfa
@@ -399,21 +181,106 @@ begin
 	Foldable(string("Show all suggested changes: "), Markdown.parse(all))
 end 
 
-# ╔═╡ ce01f106-a19f-11eb-12e3-cd894166023d
-begin 
-	function get_space()
-	ranges = Dict(String(feature) => max(1.0, Float64(maximum(col)-minimum(col))) 
-			for (feature, col) in pairs(eachcol(X)))
-	maxs = Dict(String(feature) => Float64(maximum(col)) 
-			for (feature, col) in pairs(eachcol(X)))
-	mins = Dict(String(feature) => Float64(minimum(col)) 
-			for (feature, col) in pairs(eachcol(X)))
-		return Dict("ranges" => ranges, "maxs" => maxs,"mins" => mins)
+# ╔═╡ 0bc25628-9d29-11eb-2d22-070e269ea036
+begin
+	function generate_explantions(X, classifier, p, K)
+		
+		# the result list -> one for each feature
+		num_rows = nrow(X)
+		num_features = ncol(X)
+		
+		weights = zeros(num_features)
+		
+		counts = zeros(num_features)
+		pos_counts = zeros(num_features)
+		neg_counts = zeros(num_features)
+		
+		cum_change = zeros(num_features)
+		pos_cum_change = zeros(num_features)
+		neg_cum_change = zeros(num_features)
+		
+
+		preds = MLJ.pdf.(MLJ.predict(classifier, X),1)
+		instances = X[preds .<= 0.5, :][1:100, :]
+
+		for cur_ins in eachrow(instances)
+			explanations,  = explain(cur_ins, X, p, classifier)
+			for i in 1:K
+				explanation = explanations[i, :]
+				indices = findall(explanation[:mod])
+				for index in indices
+					weights[index] += 1.0/length(indices)
+					counts[index] += 1
+					cum_change[index] += abs(cur_ins[index] - explanation[index])
+					if (explanation[index] > cur_ins[index])
+						pos_counts[index] += 1
+						pos_cum_change[index] += 
+							abs(cur_ins[index] - explanation[index])
+					else
+						neg_counts[index] += 1
+						neg_cum_change[index] += 
+							abs(cur_ins[index] - explanation[index])
+					end
+
+				end
+			end
+		end
+		
+		return instances, weights, counts, cum_change, pos_counts, neg_counts, 
+			pos_cum_change, neg_cum_change
 	end
-	space = get_space();
+
+	instances, weights, counts, cum_change, pos_counts, neg_counts, pos_cum_change, neg_cum_change = generate_explantions(X, classifier, plaf_prog, 5)
 	
-	md"feature space"
+	 
+	md""" ## Explanations for multiple instances
+	"""
 end
+
+# ╔═╡ 1113b964-9d29-11eb-19db-d1c5cb43d340
+instances
+
+# ╔═╡ a5043111-1600-4d6d-ac55-185d124abca9
+Plots.bar(1:length(counts), counts,
+	xticks=(1:length(counts), names(instance)),
+	xrotation = 30,
+	framestyle = :box,
+	ylabel="Frequency",
+	label="",
+	title="Total Count of Changes for Top 5 Explanations", 
+	)
+
+# ╔═╡ 1954ed36-7393-4ff6-843a-17512f79a92b
+
+groupedbar([pos_counts neg_counts],
+	xticks=(1:length(counts), names(instance)),
+	xrotation = 30,
+	framestyle = :box,
+	ylabel="Frequency",
+	label=["Positive Change" "Negative Change"],
+	title="Total Count of Change Directions for Top 5 Explanations",
+	)
+
+
+# ╔═╡ 8f29bd6c-9d29-11eb-0e06-d9b5178cbe33
+let 
+	ave_change = [
+		(counts[i] == 0) ? 0 : cum_change[i]/counts[i] 
+		for i in 1:length(instance)
+	]
+
+	Plots.bar(1:length(ave_change), ave_change,
+	xticks=(1:length(ave_change), names(instance)),
+	xrotation = 30,
+	framestyle = :box,
+	ylabel="change value",
+	title="Average Absolute Change",
+	label="",
+	)
+end
+
+# ╔═╡ ce01f106-a19f-11eb-12e3-cd894166023d
+space = get_space(X);
 
 # ╔═╡ e99670d2-a1a8-11eb-3cbb-49366adb2667
 let 
@@ -434,7 +301,7 @@ let
 		)
 end
 
-# ╔═╡ 95028f46-a1a9-11eb-276c-313e5d8bb515
+# ╔═╡ 30ba680e-a75a-11eb-0f4e-c51eaabb6944
 # pos_counts, neg_counts, pos_cum_change, neg_cum_change
 let 
 	namesX = names(instance)
@@ -464,7 +331,7 @@ let
 		framestyle = :box,
 		ylabel="l1-distance",
 		key=:outertopright,
-		title="Average Change w.r.t. Feature Range",
+		title="Average Change with Direction w.r.t. Feature Range",
 		label=["Positive Change" "Negative Change"],
 		)
 end
@@ -473,9 +340,7 @@ end
 begin 
 	feature_dict = Dict(feature => f_index 
 		for (f_index, feature) in 	enumerate(names(orig_instance)));
-		
-	md" feature_dict "
-end
+end;
 
 # ╔═╡ b342661f-3acc-4619-9593-fdd0f1014858
 let 
@@ -518,160 +383,6 @@ let
 	Foldable(string("Show plot: "), p)
 end
 
-# ╔═╡ 06dbd968-7453-4226-aaea-1580819f8ec3
-begin 
-	function generate_group_action(goodness, explanations, actions, user_input, K)
-		out = ""
-		if  (!goodness)
-			groups = get_group(explanations, K, user_input)
-			for group in groups
-				features = String.(names(group))
-				out *= "\\\n**COUNTERFACTUAL GROUP : $(features)**\\\n"
-				for r_index in 1:nrow(group)
-					cf = group[r_index,:]
-					out *= " -- COUNTERFACTUAL $(r_index)\\\n"
-					for feature in features
-						out *= "$(feature) ： $(user_input[feature_dict[feature]]) \$\\to\$ $(cf[feature])\\\n"
-					end
-				end
-			end
-		end
-		return out
-	end;
-	md"generate group action"
-end
-
-# ╔═╡ c8b1d8ee-8517-4e79-b1fa-429f659da38e
-# begin
-# 	lindex = 3; uindex=5
-# 	md"""
-# 	### Top Counterfactual Explanations
-# 	Number of actions to display: $(@bind actions2 NumberField(1:100000; default=6))
-# 	$(@bind actions3 NumberField(actions2:100000; default=5))
-# 	"""
-# end
-
-# ╔═╡ 498293d7-58aa-4be1-90de-d0b5ce5c8b42
-#= html"""<b style="font-family:'JuliaMono';">Counterfactual</b> 
-<details close>
-<summary style="font-family:JuliaMono;">Want to ruin the surprise?</summary>
-....
-</details>""" =#
-
-# ╔═╡ 0a72330b-27e2-494e-bbfc-c0fc6cff4d94
-md"### Previous Group Function"
-
-# ╔═╡ c84900c4-9d28-11eb-11dc-7bdfed0e91af
-md"""
-### Summary of Generated Explanations
-
-Number of groups： $(@bind K_PLAF NumberField(1:100000; default=3))
-"""
-
-# ╔═╡ d5beb41a-9d28-11eb-2f1f-69bcc72b2005
-Markdown.parse(
-	generate_group_action(false, explanations, actions, orig_instance, K_PLAF)
-)
-
-# ╔═╡ 4d0dcd7a-a1a1-11eb-305a-fb9789300d55
-let 
-	if  (!goodness)
-		plot_ret = nothing
-		count = 1
-		groups = get_group(explanations, K_PLAF, instance)
-
-		for group in groups #  index in 0:length(groups)-1
-			row_index = 1
-			features = String.(names(group))
-			xs = Array{String}(undef, nrow(group)*length(features))
-			ys = []
-			
-			for r_index in 1:DataFrames.nrow(group)
-				cf = group[r_index,:]
-				for feature in features
-					xs[row_index] = string(feature_dict[feature])
-					row_index += 1
-					append!(ys, cf[feature])
-				end
-			end
-			ori_xs = Array{String}(undef,length(features))
-			ori_ys = []
-			row_index = 1
-			for feature in features
-				ori_xs[row_index] = string(feature_dict[feature])
-				append!(ori_ys, user_input[feature_dict[feature]])
-				row_index += 1
-			end
-			
-			p_cur = Plots.scatter(xs, ys, label = "cf")
-			scatter!(ori_xs, ori_ys, label = "ori")
-			if count == 1
-				plot_ret = p_cur 
-			elseif count == length(groups)
-				plot_ret = Plots.plot(plot_ret, p_cur, 
-					layout = Plots.grid(1, 2, widths=[1-1/count, 1/count]))
-			else
-				plot_ret = Plots.plot(plot_ret, p_cur, 
-					layout = Plots.grid(1, 2, widths=[1-1/count, 1/count]))
-			end
-			count += 1
-			
-		end
-		plot_ret
-		
-	end
-end;
-
-# ╔═╡ 71f98784-a1a4-11eb-2afd-b70324f6ade1
-let 
-	if  (!goodness)
-		plot_ret = nothing
-		count = 1
-		groups = get_group(explanations, K_PLAF, instance)
-
-		for group in groups # index in 0:length(groups)-1
-			row_index = 1
-			features = String.(names(group))
-			xs = Array{String}(undef, nrow(group)*length(features))
-			ys = []
-			
-			for r_index in 1:DataFrames.nrow(group)
-				cf = group[r_index,:]
-				for feature in features
-					xs[row_index] = string(feature_dict[feature])
-					row_index += 1
-					append!(ys, (cf[feature]-space["mins"][feature]) / space["ranges"][feature])
-				end
-			end
-			ori_xs = Array{String}(undef,length(features))
-			ori_ys = []
-			row_index = 1
-			for feature in features
-				ori_xs[row_index] = string(feature_dict[feature])
-				append!(ori_ys, 
-					(user_input[feature_dict[feature]]-space["mins"][feature]) / 
-					space["ranges"][feature])
-				row_index += 1
-			end
-			
-			p_cur = Plots.scatter(xs, ys, label = "")
-			scatter!(ori_xs, ori_ys, label = "")
-			if count == 1
-				plot_ret = p_cur 
-			elseif count == length(groups)
-				plot_ret = Plots.plot(plot_ret, p_cur, 
-					layout = Plots.grid(1, 2, widths=[1-1/count, 1/count]))
-			else
-				plot_ret = Plots.plot(plot_ret, p_cur, 
-					layout = Plots.grid(1, 2, widths=[1-1/count, 1/count]))
-			end
-			count += 1
-			
-		end
-		plot_ret
-	end
-end
-
 # ╔═╡ Cell order:
 # ╟─baadbc4c-7ba3-11eb-3cbb-a32fc7755748
 # ╟─14adad40-6af7-4389-ae3a-b0d2a1462424
@@ -691,23 +402,12 @@ end
 # ╟─997132af-d04c-4745-a42a-2563e07b5ca6
 # ╟─3245072c-e91e-418c-b8ce-63386333da8c
 # ╟─b342661f-3acc-4619-9593-fdd0f1014858
-# ╟─0bc25628-9d29-11eb-2d22-070e269ea036
+# ╠═0bc25628-9d29-11eb-2d22-070e269ea036
 # ╠═1113b964-9d29-11eb-19db-d1c5cb43d340
 # ╟─a5043111-1600-4d6d-ac55-185d124abca9
 # ╟─1954ed36-7393-4ff6-843a-17512f79a92b
 # ╟─8f29bd6c-9d29-11eb-0e06-d9b5178cbe33
 # ╟─e99670d2-a1a8-11eb-3cbb-49366adb2667
-# ╟─80f49634-9d2b-11eb-359c-032422eaa82a
-# ╟─95028f46-a1a9-11eb-276c-313e5d8bb515
-# ╟─95490a61-0aa7-40f0-aa78-da9a2db11852
-# ╟─a70ab516-913b-11eb-2f4c-5b3e31e2d669
+# ╟─30ba680e-a75a-11eb-0f4e-c51eaabb6944
 # ╟─ce01f106-a19f-11eb-12e3-cd894166023d
-# ╟─06dbd968-7453-4226-aaea-1580819f8ec3
 # ╟─479acc12-915c-11eb-1a4b-f588eb756b0d
-# ╠═c8b1d8ee-8517-4e79-b1fa-429f659da38e
-# ╠═498293d7-58aa-4be1-90de-d0b5ce5c8b42
-# ╟─0a72330b-27e2-494e-bbfc-c0fc6cff4d94
-# ╟─c84900c4-9d28-11eb-11dc-7bdfed0e91af
-# ╟─d5beb41a-9d28-11eb-2f1f-69bcc72b2005
-# ╟─4d0dcd7a-a1a1-11eb-305a-fb9789300d55
-# ╟─71f98784-a1a4-11eb-2afd-b70324f6ade1
